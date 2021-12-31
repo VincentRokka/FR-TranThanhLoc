c7cf065d6ab2a123afff0f8bd8e1a3b98ddb5068
Thanh Loc Tran <muzukagede0@gmail.com>
2021-12-31 13:55:42 +0700

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Program 
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Name: ");
+            string yourName = Console.ReadLine();
+
+            Console.WriteLine("Hello <" + yourName + " >!");
+            Console.WriteLine("Welcome to NTQ Solution");
+            Console.ReadKey();
+        }
+    }
+}
