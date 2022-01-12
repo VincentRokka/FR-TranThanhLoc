30f293dc69b649712943eb8e5b79b53c615827de
Tran Thanh Loc <muzukagede0@gmail.com>
2022-01-12 17:39:53 +0700

    Update OOP exercise

---
+++ OOP/Employee.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Employee
+    {
+        private string _id;
+        public string ID
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        private double _salaryOneHour;
+        public double SalaryOneHour
+        {
+            get { return _salaryOneHour; }
+            set { _salaryOneHour = value; }
+        }
+        private int _workingHour;
+        public int WorkingHour
+        {
+            get { return _workingHour; }
+            set { _workingHour = value; }
+        }
+
+        public Employee() { }
+
+        public Employee(string id, string name, double salary, int workingHour)
+        {
+            this.ID = id;
+            this.Name = name;
+            this.SalaryOneHour = salary;
+            this.WorkingHour = workingHour;
+        }
+
+        public double SumSalary()
+        {
+            return this.SalaryOneHour * this.WorkingHour;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("\n{0,-15} {1,-20} {2, -10} {3, -15} {4 , -20}", this.ID, this.Name, this.SalaryOneHour, this.WorkingHour, this.SumSalary());
+        }
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OOP
+{
+    class Program
+    {
+        static private List<Employee> ListEmployees = new List<Employee>();
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("1. Enter Employee Information");
+                Console.WriteLine("2. Show List Employees");
+                Console.WriteLine("3. Exit");
+                string choose = Console.ReadLine();
+
+                switch (choose)
+                {
+                    case "1":
+                        enterEmployeeInformation();
+                        break;
+                    case "2":
+                        Console.WriteLine("\n{0,-15} {1,-20} {2, -10}", "ID", "Name", "Salary");
+                        showListEmployees();
+                        Console.ReadLine();
+                        break;
+                    case "3":
+                        return;
+                    default:
+                        Console.WriteLine("\nError, Please Repeat:");
+                        Console.ReadLine();
+                        break;
+                }
+            } while (true);
+        }
+        static private void enterEmployeeInformation()
+        {
+            string id, name;
+            double salary;
+            int workingHour;
+
+            Console.WriteLine("Enter ID: ");
+            id = Console.ReadLine();
+            Console.WriteLine("Enter Name: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter Salary of 1 Hour: ");
+            salary = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Working Hour of Employee: ");
+            workingHour = int.Parse(Console.ReadLine());
+
+            Employee NewEmployee = new Employee(id, name, salary, workingHour);
+            ListEmployees.Add(NewEmployee);
+
+            Console.WriteLine("Enter to Continue");
+            Console.ReadLine();
+        }
+        static private void showListEmployees()
+        {
+            foreach (Employee i in ListEmployees)
+            {
+                Console.WriteLine(i.ID.ToString() + "\t " + i.Name.ToString() + "\t " + i.SumSalary().ToString());
+            }
+        }
+
+    }
+}
